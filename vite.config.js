82d9f4464a28d03b6b38e1bacf961e2f8d02c09c
Kaoru Kato <kaoru.kato.2003@icloud.com>
2025-03-11 03:16:25 +0900

    デプロイ設定

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "js/dist", // ← ビルドの出力先をAzureの期待するパスに変更
+    outDir: "dist", // ← ビルドの出力先をAzureの期待するパスに変更
   },
 });
